b4fbde42a5558aef0ac1dee0356f45272c8065b0
Madison Hess <hessmad@uw.edu>
2018-05-13 20:36:23 -0700

    Add functions

--- analysis.R
+++ analysis.R
@@ -6,11 +6,15 @@ intro_survey <- read.csv("data/intro-survey.csv", stringsAsFactors = FALSE)
 colnames(intro_survey)
 num_students <- nrow(intro_survey)
 
-# Function 1: Goup by year in college
-by_year <- function() {
-  intro_survey %>%
-    group_by(What.is.your.current.class.standing.) %>%
-    tally()
+# Summary function
+intro_function <- function(dataset) {
+  ret <- list()
+  ret$`Number of Students` <- nrow(dataset)
+  ret$`Average Height` <- mean(dataset$How.many.inches.tall.are.you., na.rm = TRUE)
+  ret$`Class Standing: Freshman` <- dataset %>% filter(What.is.your.current.class.standing. == "Freshman") %>% tally()
+  ret$`Class Standing: Sophomore` <- dataset %>% filter(What.is.your.current.class.standing. == "Sophomore") %>% tally()
+  ret$`Class Standing: Junior` <- dataset %>% filter(What.is.your.current.class.standing. == "Junior") %>% tally()
+  ret$`Class Standing: Senior` <- dataset %>% filter(What.is.your.current.class.standing. == "Senior") %>% tally()
+  return (ret)
 }
-# Find average height and make function related to height
-avg_height <- mean(intro_survey$How.many.inches.tall.are.you., na.rm = TRUE)
+
